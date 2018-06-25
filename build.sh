24bbf04282f8e18890fd12c004d66607e4c82e1b
Patricio Gonzalez <patricio.gonzalez@idiomsoftware.com>
2018-06-25 13:24:56 +1200

    Love Travis ...

--- build.sh
+++ build.sh
@@ -3,16 +3,18 @@
 #exit if any command fails
 set -e
 
+cd $TRAVIS_BUILD_DIR/Website
+
 artifactsFolder="./artifacts"
 
 if [ -d $artifactsFolder ]; then  
   rm -R $artifactsFolder
 fi
 
-dotnet restore Website\Website.sln
-dotnet build Website\Website.sln -c Release
+dotnet restore Website.sln
+dotnet build Website.sln -c Release
 
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
 
-dotnet pack Website\Website.sln -c Release -o $artifactsFolder --version-suffix=$revision  
+dotnet pack Website.sln -c Release -o $artifactsFolder --version-suffix=$revision  
